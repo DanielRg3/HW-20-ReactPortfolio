118986d37c50d8eb2c99899693568d3008d12db0
Daniel Ruiz <danielruizgtz@gmail.com>
2021-11-23 21:47:50 -0600

    In this commit I'm updating the App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Cover from "./components/cover/Cover";
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return <div className="App">
+    <Navbar />
     <Cover />
   </div>;
 }
