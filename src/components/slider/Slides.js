0f33dcff18350468bfadc43eac726bdb01552a94
Daniel Ruiz <danielruizgtz@gmail.com>
2021-11-24 18:42:29 -0600

    Final Commit adding the urls to my projects

--- src/components/slider/Slides.js
+++ src/components/slider/Slides.js
@@ -7,27 +7,32 @@ const slidesInfo = [
       "https://cdn.pixabay.com/photo/2016/12/19/08/39/mobile-phone-1917737_960_720.jpg",
     alt: "Password Generator",
     desc: "Project 1: Password Generator",
+    URL: "https://danielrg3.github.io/PasswordGeneratorHW03/",
   },
   {
     src:
       "https://cdn.pixabay.com/photo/2017/10/31/19/05/web-design-2906159_960_720.jpg",
     alt: "Project 2",
     desc: "Project 2: Quiz with Timer",
+    URL: "https://danielrg3.github.io/Homework04",
   },
   {
     src:
       "https://cdn.pixabay.com/photo/2015/02/05/08/06/macbook-624707_960_720.jpg",
     alt: "Project 3",
     desc: "Project 3: Budget App",
+    URL: "https://danielrg3.github.io/DanielsPortfolioHW2",
   },
 ];
 
 const slides = slidesInfo.map((slide) => (
   <div className="slide-container">
-    <img src={slide.src} alt={slide.alt} />
-    <div className="slide-desc">
-      <span>{slide.desc}</span>
-    </div>
+    <a href={slide.URL} target="_blank">
+      <img src={slide.src} alt={slide.alt} />
+      <div className="slide-desc">
+        <span>{slide.desc}</span>
+      </div>
+    </a>
   </div>
 ));
 
